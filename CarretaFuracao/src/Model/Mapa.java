b4030177f291c8add8c6916b169e3b875cff694b
victor <victor.freitas.3005@gmail.com>
2016-04-14 09:31:50 -0300

    Mapa dos trilhos

---
+++ CarretaFuracao/src/Model/Mapa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import com.sun.glass.ui.Cursor;
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author victor
+ */
+public class Mapa extends JFrame {
+
+    public Mapa() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Mapa m = new Mapa();
+
+    }
+
+    public void inserirTrem(){
+        new Trem();
+    }
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.drawRect(150, 300, 250, 250);
+        g2d.drawRect(400, 300, 250, 250);
+        g2d.drawRect(275, 175, 250, 125);
+        inserirTrem();
+
+    }
+
+}
