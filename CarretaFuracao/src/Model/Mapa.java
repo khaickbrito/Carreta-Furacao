8bb5b977c22e86d06fb6d51deb5d97abc811bdf2
khaickbrito <khaickbrito@gmail.com>
2016-05-01 17:34:08 -0300

    Tudo ai

--- CarretaFuracao/src/Model/Mapa.java
+++ CarretaFuracao/src/Model/Mapa.java
@@ -18,31 +18,40 @@ import javax.swing.JFrame;
  * @author victor
  */
 public class Mapa extends JFrame {
-
+    private static Trem trem1;
+    private static Trem trem2;
+    private static Trem trem3;
+    
+    
     public Mapa() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
-        
         setVisible(true);
     }
 
     public static void main(String[] args) {
         Mapa m = new Mapa();
+        trem1 = new Trem(520, 270, 295, 170, m, 270, 170);
+        trem2 = new Trem(395, 145, 545, 295, m, 145, 545);
+        trem3 = new Trem(645, 395, 545, 295, m, 645, 545);
+
+//        m.add(trem1.getBotao());
+//        trem1.getBotao().setVisible(true);
+//        trem1.getBotao().setSize(100, 50);
 
     }
-
-    public void inserirTrem(Graphics g){
-        Trem t = new Trem(g, 270, 170);
-        new Thread(t).start();
-    }
+    
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.drawRect(150, 300, 250, 250);
         g2d.drawRect(400, 300, 250, 250);
         g2d.drawRect(275, 175, 250, 125);
-        inserirTrem(g2d);
-
+        g2d.setColor(Color.RED);
+        g2d.fillOval(trem1.x, trem1.y, 10, 10);
+        g2d.fillOval(trem2.x, trem2.y, 10, 10);
+        g2d.fillOval(trem3.x, trem3.y, 10, 10);
     }
 
 }
--- CarretaFuracao/src/Model/Trem.java
+++ CarretaFuracao/src/Model/Trem.java
@@ -10,48 +10,84 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Transparency;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import static java.lang.Thread.sleep;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicArrowButton;
 
 /**
  *
  * @author victor
  */
-public class Trem extends JComponent implements Runnable {
+public class Trem{
+    private int maxX;
+    private int minX;
+    private int maxY;
+    private int minY;
+    private Mapa mapa;
+    public int x;
+    public int y;
+    public long velocidade = 15;
+    private int cont;
+    private JButton botao;
+    private JTextField campoVelocidade;
 
-    private int x;
-    private int y;
-    private Graphics g2d;
-    public Trem(Graphics g, int x, int y) {
+    public Trem(int maxX, int minX, int maxY, int minY, Mapa mapa, int x, int y) {
+        this.maxX = maxX;
+        this.minX = minX;
+        this.maxY = maxY;
+        this.minY = minY;
+        this.mapa = mapa;
         this.x = x;
         this.y = y;
-        Graphics2D g2d = (Graphics2D) g.create();
-        this.g2d = g2d;
+        new Thread(new TremThread()).start();
         
+//        campoVelocidade = new JTextField();
+//        campoVelocidade.setText("Digite");
+//        botao = new JButton();
+//        botao.setText("Alterar");
+//        botao.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                velocidade = Long.parseLong(campoVelocidade.getText());
+//            }
+//        });
+    }
+
+    public JButton getBotao() {
+        return botao;
+    }
+
+    public JTextField getCampoVelocidade() {
+        return campoVelocidade;
     }
     
-   
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 250; i++) {
-            g2d.setColor(Color.RED);
-            g2d.fillRoundRect(this.x, this.y, 10, 10, 100, 100);
-            
-            repaint();
-            this.x += 1;
-            try {
-                sleep(10);
-                
-                //g2d.setColor(Color.BLACK);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Trem.class.getName()).log(Level.SEVERE, null, ex);
+    private class TremThread implements Runnable{
+        
+        @Override
+        public void run() {
+            while(true){
+                if(x != maxX && y == minY){
+                    x++;
+                }else if(x != minX && y == maxY){
+                    x--;
+                }else if(x == maxX && y != maxY){
+                    y++;
+                }else if(x == minX && y != minY){
+                    y--;
+                }
+                mapa.repaint();
+                try {
+                    sleep(velocidade);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Trem.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
-        
-        
     }
-
 }
